7653606bf549aab5dd2076964671001b73b45336
camthegeek <cam@camthegeek.net>
2020-06-12 11:33:47 -0400

    [wallet2] ring_size min/max fix

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -7364,20 +7364,14 @@ int wallet2::get_fee_algorithm()
 uint64_t wallet2::get_min_ring_size()
 {
   if (use_fork_rules(8, 10))
-    return 11;
-  if (use_fork_rules(7, 10))
-    return 7;
-  if (use_fork_rules(6, 10))
-    return 5;
-  if (use_fork_rules(2, 10))
-    return 3;
+    return 15;
   return 0;
 }
 //------------------------------------------------------------------------------------------------------------------------------
 uint64_t wallet2::get_max_ring_size()
 {
   if (use_fork_rules(8, 10))
-    return 11;
+    return 15;
   return 0;
 }
 //------------------------------------------------------------------------------------------------------------------------------
